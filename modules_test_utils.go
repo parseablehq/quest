daec028658ec65e33df6fe2e649ccce384e4e42c
Aldrin Jenson <aldrinjenson@gmail.com>
2023-10-12 19:53:05 +0530

    Update and receive config + test duplicate deregister

--- modules_test_utils.go
+++ modules_test_utils.go
@@ -29,6 +29,17 @@ const sampleModuleRegistrationJSON = `{
   ]
 }`
 
+const sample_module_config_per_stream = `
+[{
+    "mode": "col",
+    "algorithm": "z-score",
+    "start_time": "2023-10-12T08:38:47.069Z",
+    "end_time": "2023-10-12T08:40:47.069Z",
+    "interval_window": "5s",
+    "column_to_watch": "api_timeToResponseInNS"
+}]
+`
+
 func test_module_registration_flow(t *testing.T) error {
 
 	start_server()
@@ -50,9 +61,16 @@ func test_module_registration_flow(t *testing.T) error {
 	require.Equalf(t, 200, response.StatusCode, "Server returned http code: %s resp %s", response.Status, readAsString(response.Body))
 
 	println("Updating config")
-	req, _ = NewGlob.Client.NewRequest("PUT", "modules/"+module_name+"/config/"+stream_name, bytes.NewBufferString("[]"))
+	req, _ = NewGlob.Client.NewRequest("PUT", "modules/"+module_name+"/config/"+stream_name, bytes.NewBufferString(sample_module_config_per_stream))
 	response, err = NewGlob.Client.Do(req)
 	require.NoErrorf(t, err, "Request failed: %s", err)
+	require.Equalf(t, 200, response.StatusCode, "Server returned http code: %s resp %s", response.Status, readAsString(response.Body))
+
+	println("Receiving config")
+	req, _ = NewGlob.Client.NewRequest("GET", "modules/"+module_name+"/config/"+stream_name, bytes.NewBufferString("{}"))
+	response, err = NewGlob.Client.Do(req)
+	require.NoErrorf(t, err, "Request failed: %s", err)
+	// require.Equalf(t, bytes.NewBufferString((sample_module_config_per_stream)), response.Body, "Server returned http code: %s resp %s", response.Status, readAsString(response.Body))
 	require.Equalf(t, 200, response.StatusCode, "Server returned http code: %s resp %s", response.Status, readAsString(response.Body))
 
 	stop_server()
@@ -62,6 +80,12 @@ func test_module_registration_flow(t *testing.T) error {
 	response, err = NewGlob.Client.Do(req)
 	require.NoErrorf(t, err, "Request failed: %s", err)
 	require.Equalf(t, 200, response.StatusCode, "Server returned http code: %s resp %s", response.Status, readAsString(response.Body))
+
+	println("Testing Duplicate DeRegister ")
+	req, _ = NewGlob.Client.NewRequest("DELETE", "modules/"+module_name, bytes.NewBufferString("{}"))
+	response, err = NewGlob.Client.Do(req)
+	require.NoErrorf(t, err, "Request failed: %s", err)
+	require.Equalf(t, 400, response.StatusCode, "Server returned http code: %s resp %s", response.Status, readAsString(response.Body))
 
 	return nil
 }
