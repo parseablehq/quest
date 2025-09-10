23ec17e07b6ad7bbf6887fc868f8743740dd8ad5
Nikhil Sinha <nikhil.sinha@parseable.com>
2025-09-09 21:52:39 -0700

    update test for user with and without role

--- test_utils.go
+++ test_utils.go
@@ -312,7 +312,7 @@ func CreateUser(t *testing.T, client HTTPClient, user string) string {
 	response, err := client.Do(req)
 	require.NoErrorf(t, err, "Request failed: %s", err)
 	body := readAsString(response.Body)
-	require.Equalf(t, 400, response.StatusCode, "Server returned http code: %s resp %s", response.Status, readAsString(response.Body))
+	require.Equalf(t, 200, response.StatusCode, "Server returned http code: %s resp %s", response.Status, readAsString(response.Body))
 	return body
 }
 
